63e8d89829281a2f4f00bcae7e72c8382f4b2653
Gabriel <gabrielpabalesalazar@gmail.com>
2024-05-03 00:04:03 +0800

    Add project files.

---
+++ PREFINALS_AUTH_Salazar_GabrielAngelo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PREFINALS_AUTH_Salazar_GabrielAngelo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
