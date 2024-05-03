ccdcf7a016b8594eca0edc9e08d2f4da5f2e987e
Gabriel <gabrielpabalesalazar@gmail.com>
2024-05-03 18:31:38 +0800

    Created and Controller Api for Auth

---
+++ AuthServer/Controllers/AuthController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AuthServer.Core.Service;
+using static AuthServer.Core.Service.IUserServices;
+
+namespace AuthServer.Presentation.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly IAuthService _authService;
+
+        public AuthController(IUserService userService, IAuthService authService)
+        {
+            _userService = userService;
+            _authService = authService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(string username, string password)
+        {
+            if (await _userService.RegisterAsync(username, password))
+                return Ok();
+
+            return BadRequest();
+        }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> LoginAsync(string username, string password)
+        {
+            var token = await _userService.AuthenticateAsync(username, password);
+            if (token != null)
+                return Ok(new { Token = token });
+
+            return Unauthorized();
+        }
+    }
+}
+
