e9d1e2c24e98ed56f11e70493cbad9084301edd8
Gabriel <gabrielpabalesalazar@gmail.com>
2024-05-03 18:02:16 +0800

    Created an Repositories for the User

---
+++ AuthServer/Core/Repositories/IUserRepositories.cs
@@ -0,0 +1,12 @@
+﻿using AuthServer.Core.Model;
+using System.Threading.Tasks;
+
+namespace AuthServer.Domain.Repositories
+{
+    public interface IUserRepositories
+    {
+        Task<User> GetUserAsync(string username);
+        Task<bool> AddUserAsync(User user);
+    }
+}
+
---
+++ AuthServer/Core/Repositories/UserRepositories.cs
@@ -0,0 +1,40 @@
+﻿using AuthServer.Core.Model;
+using AuthServer.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthServer.Repositories
+{
+    public class UserRepositories : IUserRepositories
+    {
+        private readonly List<User> _users;
+
+        public UserRepositories()
+        {
+            _users = new List<User>();
+        }
+
+        public Task<User> GetUserAsync(string username)
+        {
+            var user = _users.FirstOrDefault(u => u.Username == username);
+            return Task.FromResult(user);
+        }
+
+        public Task<bool> AddUserAsync(User user)
+        {
+            try
+            {
+                user.Id = _users.Count + 1;
+                _users.Add(user);
+                return Task.FromResult(true);
+            }
+            catch (Exception ex)
+            {
+                return Task.FromResult(false);
+            }
+        }
+    }
+}
+
