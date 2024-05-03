341790fc73c3f065cec7e4d78a545c5640a95079
Gabriel <gabrielpabalesalazar@gmail.com>
2024-05-03 17:48:37 +0800

    created user.cs and added folders for the onion architecture

---
+++ AuthServer/Core/Model/User.cs
@@ -0,0 +1,11 @@
+﻿namespace AuthServer.Core.Model
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+
+    }
+}
+
